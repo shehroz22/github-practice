27b70dde702d8c6f6f264238661efa8fef586c75
shehroz22 <94064174+shehroz22@users.noreply.github.com>
2022-11-16 02:56:22 -0500

    filter using class component basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { Component } from "react";
+class App extends Component{
+
+  
+
+  constructor(){
+    super()
+// this.state={name:" hamza"}
+  // }
+this.state={monster:[
+{name:"ali",
+key:"22"},
+{name:"hamza",
+key:"23q2"},
+{name:"sikh",
+key:"23332"}
+]}
+  }
+
+
+
+render(){
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  //   <div className="App">
+  //   <h1> his name is {this.state.name}</h1>
+  //  <button type="" onClick={()=>{this.setState({name:"khan"})} }>clcik me </button>
+    
+  
+  
+  
+  // </div>
+
+<div className='App'>
+
+<input onChange={(event)=>{console.log(event.target.value)
+  const lowerCase=event.target.value.toLowerCase()
+  const destate=this.state.monster.filter((monsters)=>{return monsters.name.toLowerCase().includes(lowerCase)})
+  this.setState(()=>{ return {monster:destate}})
+
+}} type="search" name="search" placeholder='seacrh' value=""/>
+
+
+
+{this.state.monster.map((monster)=>
+
+  <h1 key={monster.key} >{monster.name}</h1>)}
+  
+</div>
+
   );
+  }
 }
 
 export default App;
